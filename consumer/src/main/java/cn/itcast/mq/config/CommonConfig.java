0723d2821838338b6dd5a8469776a02c091cda6d
xiaojiatongzhi <2652017960@qq.com>
2022-01-22 15:53:45 +0800

    jasjk

--- consumer/src/main/java/cn/itcast/mq/config/CommonConfig.java
+++ consumer/src/main/java/cn/itcast/mq/config/CommonConfig.java
@@ -12,6 +12,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CommonConfig {
     public static void main(String[] args) {
-
+        System.out.println("aaaaaaa");
     }
 }
